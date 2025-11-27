7b0dde41866e7fab145bd9947e496ccee231b4a7
Darragh Drennan (Tallaght TC Student) <d.drennan25@ddletbtc.ie>
2025-11-27 08:31:52 +0000

    Print csv to terminal to check is working

--- CSAssignment/Program.cs
+++ CSAssignment/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace Timesheet // Container name. 
+﻿using System;
+using System.IO;
+
+namespace Timesheet // Container name. 
 {
     class Program // Must be same as file name.
     {
         public static void Main(string[] args) // Main method, all executable code starts here. 
         {
-            
+            string filePath = "Resources/OrganisationWeeklyTimesheet.csv";
+            string[] data = File.ReadAllLines(filePath);
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+
         }
+
     }
 }
 
