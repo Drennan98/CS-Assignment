1a91a1657adf9b4a599bcbde4938387865687c56
Darragh Drennan (Tallaght TC Student) <d.drennan25@ddletbtc.ie>
2025-11-27 09:27:13 +0000

    Create number of employees variable and minus 1 to ignore heading of csv

--- CSAssignment/Program.cs
+++ CSAssignment/Program.cs
@@ -7,14 +7,10 @@ namespace Timesheet // Container name.
     {
         public static void Main(string[] args) // Main method, all executable code starts here. 
         {
-            string filePath = "Resources/OrganisationWeeklyTimesheet.csv";
-            string[] data = File.ReadAllLines(filePath);
+            string fileLines = "Resources/OrganisationWeeklyTimesheet.csv"; // Reading in csv file. 
+            int numberOfEmployees = fileLines.Length - 1; // Calculating how many employees are in file and we subtract 1 as we don't need the heading. 
 
-            for(int i = 0; i < data.Length; i++)
-            {
-                Console.WriteLine(data[i]);
-            }
-
+            Employee[] employees = new Employee[numberOfEmployees]; 
         }
 
     }
