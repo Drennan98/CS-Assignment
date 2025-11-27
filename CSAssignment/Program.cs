e2fca804aff884ed5bf95106107338a887e90e6f
Darragh Drennan (Tallaght TC Student) <d.drennan25@ddletbtc.ie>
2025-11-27 09:59:15 +0000

    Create day variables and parse them into doubles

--- CSAssignment/Program.cs
+++ CSAssignment/Program.cs
@@ -7,23 +7,33 @@ namespace Timesheet // Container name.
     {
         public static void Main(string[] args) // Main method, all executable code starts here. 
         {
-            string[] fileLines = File.ReadAllLines("OrganisationWeeklyTimesheet.csv"); // Reading in csv file. 
+            string[] fileLines = File.ReadAllLines("./Resources/OrganisationWeeklyTimesheet.csv"); // Reading in csv file. s
             int numberOfEmployees = fileLines.Length - 1; // Calculating how many employees are in file and we subtract 1 as we don't need the heading. 
 
             Employee[] employees = new Employee[numberOfEmployees]; // Array for holding number of employees. 
 
-            int arrayIndex = 0; // To keep count of where we are filling employee array. 
+            // int arrayIndex = 0; // To keep count of where we are filling employee array. 
 
             for (int i = 1; i < fileLines.Length; i++)
             {
                 string[] columns = fileLines[i].Split(","); // Gives an array of strings called columns split by ,. 
 
-                Employee employee = new Employee();
+                Employee newEmployee = new Employee(); // New instance of employee. 
+
+                newEmployee.Name = columns[0]; // New employee name which is given index 0 as Name is first heading in our array of columns. 
+                newEmployee.Department = columns[1]; // New employee Department which is given index 1 as Department is second heading in our array of columns. 
+
+                double mon = double.Parse(columns[2]);
+                double tues = double.Parse(columns[3]);
+                double wed = double.Parse(columns[4]);
+                double thurs = double.Parse(columns[5]);
+                double fri = double.Parse(columns[6]);
+
             }
+        }
         }
 
     }
-}
 
 
 
--- CSAssignment/obj/Debug/net9.0/CSAssignment.AssemblyInfo.cs
+++ CSAssignment/obj/Debug/net9.0/CSAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a91a1657adf9b4a599bcbde4938387865687c56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35aef672b2c27e610edcd1c6145e42df6f558093")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
